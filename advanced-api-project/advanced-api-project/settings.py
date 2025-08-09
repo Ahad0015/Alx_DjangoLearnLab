ad117d79b63da6091fe43bbf8a3af9eaebb37f2e
Ahad0015 <aah74500@gmail.com>
2025-08-09 09:56:03 -0700

    Add rest_framework, models and serializers

--- advanced-api-project/advanced-api-project/settings.py
+++ advanced-api-project/advanced-api-project/settings.py
@@ -83,6 +83,11 @@ DATABASES = {
     }
 }
 
+INSTALLED_APPS = [
+    ...,
+    'rest_framework',
+    'api',
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
