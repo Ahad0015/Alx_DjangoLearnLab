62d7f9795a725aaefa000b0111b8a6b9d0f2253f
Ahad0015 <aah74500@gmail.com>
2025-08-24 11:25:32 -0700

    social media api

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,41 +1,30 @@
-from django.shortcuts import render
-
-# Create your views here.
 from rest_framework import generics, permissions
-from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
-from rest_framework.views import APIView
-from django.contrib.auth import authenticate, get_user_model
+from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.views import ObtainAuthToken
+from django.contrib.auth import get_user_model
 from .serializers import RegisterSerializer, UserSerializer
 
 User = get_user_model()
 
-# Register
+# Registration View
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
 
-    def create(self, request, *args, **kwargs):
-        response = super().create(request, *args, **kwargs)
-        user = User.objects.get(username=response.data['username'])
-        token = Token.objects.get(user=user)
-        response.data['token'] = token.key
-        return response
+# Login View (token-based)
+class LoginView(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        response = super().post(request, *args, **kwargs)
+        token = Token.objects.get(key=response.data["token"])
+        return Response({
+            "token": token.key,
+            "user_id": token.user_id,
+            "username": token.user.username
+        })
 
-# Login
-class LoginView(APIView):
-    def post(self, request):
-        username = request.data.get("username")
-        password = request.data.get("password")
-        user = authenticate(username=username, password=password)
-        if user:
-            token, created = Token.objects.get_or_create(user=user)
-            return Response({"token": token.key})
-        return Response({"error": "Invalid credentials"}, status=400)
-
-# Profile
+# Profile View
 class ProfileView(generics.RetrieveUpdateAPIView):
-    queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
 
